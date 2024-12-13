aaba910902c95a93906977334d5549c21a0a5b50
Felipe Alejandro Suárez Agostini <102730041+alejasu10@users.noreply.github.com>
2024-12-13 16:24:50 +0000

    iniciando en streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def prueba():
+
+    y= pd.DataFrame(
+        {
+            "Nombre": ["felipe", "juan", "carlos", "david"],
+            "Apellido": ["suarez", "aaaa", "sssss", "dddd"],
+        }
+    )
+    return y
+
+if __name__ == "__main__":
+    with st.sidebar:
+        if st.button("HOLA"):
+            st.write("hola")
+    st.title("🎈 My new app")
+    st.write(
+        "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    )
+    st.write("hola mundo")
+    x=10
+    st.write(f"mi numero es: {x}")
+    st.title("mundo")
+
+    if st.button("hola"):
+        st.write(prueba())
