30b039693fe94a27fbcda5bb82c076f0d99f2424
Felipe Alejandro Suárez Agostini <102730041+alejasu10@users.noreply.github.com>
2024-12-13 16:42:57 +0000

    act

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,21 @@ def prueba():
     return y
 
 if __name__ == "__main__":
+    pages = {
+    "Your account": [
+        st.Page("pages/CUENTAS.py", title="Create your account"),
+        st.Page("pages/INICIO.py", title="Manage your account")
+    ]
+       
+}
+
+    pg = st.navigation(pages)
+    pg.run()
+
     with st.sidebar:
-        if st.button("HOLA"):
-            st.write("hola")
+            if st.button("HOLA"):
+                st.write("hola")
+
     st.title("🎈 My new app")
     st.write(
         "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
@@ -26,3 +38,19 @@ if __name__ == "__main__":
 
     if st.button("hola"):
         st.write(prueba())
+
+    
+    st.page_link("pages/INICIO.py")
+
+    if st.button("Page 2"):
+        st.switch_page("pages/CUENTAS.py")
+
+    number=st.number_input("insert a number")
+    numb1=st.number_input("insert a number1")
+    numb2=st.number_input("insert a number2")
+    suma=int(numb1)+int(numb2)
+    if suma:
+        st.write(f"la suma es: {suma}")
+    if number:
+         st.write(f"el doble del numero es {number*2}")
+
